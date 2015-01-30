cfb370641d4d5ed49fb2a0a926a02c29abf44775
statpharmer <statpharmer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-30 01:23:09 +0000

    ff ff2 and grid + dox implemented grid dox included new examples also how to visualize 3-way interaction
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/forestfloor@59 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/forestFloor/R/forestFloor_source.R
+++ pkg/forestFloor/R/forestFloor_source.R
@@ -348,7 +348,8 @@ if(d==2) { #if 2D VEC space
 convolute_ff = function(ff,
                         these.vars=NULL,
                         k.fun=function() round(sqrt(n.obs)/2),
-                        userArgs.kknn = alist(kernel="gaussian")) {
+                        userArgs.kknn = alist(kernel="gaussian")
+                        ) {
   
   n.obs=dim(ff$X)[1]
   n.vars=dim(ff$X)[2]
@@ -369,6 +370,107 @@ convolute_ff = function(ff,
 }
 
 
+#f3 input a forestFloor object, as convolute_ff but do not iterate all variables. Instead wrapper will use
+#kknn to convolute a designated featureContribution with designated features - oftenly the corresponding features.
+convolute_ff2 = function(ff,
+                        Xvars,
+                        FCvars = NULL,
+                        k.fun=function() round(sqrt(n.obs)/2),
+                        userArgs.kknn = alist(kernel="gaussian")
+) {
+  if(is.null(FCvars)) FCvars = Xvars
+  n.obs=dim(ff$X)[1]
+  k=k.fun()
+    
+  #merge user and wrapper args
+  defaultArgs.kknn = alist(formula=fc~.,data=Data,kmax=k,kernel="gaussian")
+  kknn.args=append.overwrite.alists(userArgs.kknn,defaultArgs.kknn)
+  
+  #collect coloumns
+  if(length(FCvars)>1) {
+    fc = apply(ff$FCmatrix[,FCvars],1,sum)
+  } else {
+    fc = ff$FCmatrix[,FCvars]
+  }
+  x = ff$X[,Xvars]
+  
+  #compute topology
+    Data = data.frame(fc=fc,x=x)
+    knn.obj = do.call("train.kknn",kknn.args)$fitted.values
+    out = knn.obj[[length(knn.obj)]]
+}
+
+#f4 input a forestFloor object, as convolute_ff but do not iterate all variables. Instead wrapper will use
+#kknn to convolute a designated featureContribution with designated features - oftenly the corresponding features.
+convolute_grid = function(ff,
+                          Xvars,
+                          FCvars = NULL,
+                          grid = 30,
+                          limit = 3,
+                          zoom = 3,
+                          k.fun=function() round(sqrt(n.obs)/2),
+                          userArgs.kknn = alist(kernel="gaussian")
+) {
+  
+  #input defaults
+  if(is.null(FCvars)) FCvars = Xvars
+  n.obs=dim(ff$X)[1]
+  k=k.fun() # will be overided if a "k=" argument is provided in userArgs.kknn-list
+  
+  #collect coloumns of data
+  if(length(FCvars)>1) {
+    fc = apply(ff$FCmatrix[,FCvars],1,sum)
+  } else {
+    fc = ff$FCmatrix[,FCvars]
+  }
+  X = ff$X[,Xvars]
+  
+  #make grid, or use external grid if provided
+  if(length(grid)==1) {
+    X = box.outliers(X,limit=limit,normalize=F)
+    get.seq = function(x) {
+      upper = (max(x) - mean(x)) * zoom
+      lower = (min(x) - mean(x)) * zoom
+      seq(lower+mean(x), upper+mean(x),length.out=grid)
+    }
+    ite.val=lapply(1:dim(X)[2],function(i) get.seq(X[,i])) #for each variable define span of grid
+    gridX = as.data.frame(expand.grid(ite.val))
+    names(gridX) = names(X)
+    
+    ##WOUPS NEVERMIND it worked...
+#     #could not make list of vectors work with expand.grid as doc promises
+#     #this hack calls expand.grid with specified arguments
+#     
+#     #hack - make charecter string of appropiate code
+#     run.this = "alist("
+#     for(i in 1:dim(X)[2]) run.this = paste(run.this,names(X)[i]," = ite.val[,",i,"],",sep="")
+#     run.this = substr(run.this,1,nchar(run.this)-1)
+#     run.this = paste(run.this,")")
+#     #hack - parse and evaluate string into a argument list of one argument for each dimension
+#     arg.list = eval(parse(text=run.this))
+#     #call expand.grid with specified arguments
+#     gridX=as.data.frame(do.call(expand.grid,arg.list)) #grid coordinates
+
+  } else {
+    gridX = grid
+  }
+  
+  #prepare data
+  Data = data.frame(fc=fc,x=ff$X[,Xvars])
+  gridX = data.frame(x=gridX)
+  
+  #merge user args and args of this wrapper function, user args have priority
+  defaultArgs.kknn = alist(formula=fc~.,train=Data,k=k,kernel="gaussian",test=gridX)
+  kknn.args=append.overwrite.alists(userArgs.kknn,defaultArgs.kknn)
+    
+  #execute kknn function and retrive convolution
+  gridFC = do.call("kknn",kknn.args)$fitted.values
+  
+  #gather results in data frame, with convoluted feature contributinos in grid
+  return(data.frame(fc = gridFC,gridX))
+}
+
+
 
 #sf5 scale data and grid, to allow knn 
 scale.by = function(scale.this,by.this) {
@@ -383,20 +485,26 @@ scale.by = function(scale.this,by.this) {
 
 #sf6  reduce outliers to within limit of 1.5 std.dev and/or output as normalized 
 box.outliers = function(x,limit=1.5,normalize=T) {
+  
   sx=scale(x)
   if(limit!=FALSE) {
     sx[ sx>limit] =  limit
     sx[-sx>limit] = -limit
   }
+  
   if(normalize) { 
     sx.span = max(sx) - min(sx)
     sx = sx - min(sx)
     sx = sx / sx.span
-    return(sx)
   } else {
-    x = sx * attributes(sx)$"scaled:scale" + attributes(sx)$"scaled:center"
-    return(x)
+    sx = sx * attributes(sx)$"scaled:scale" + attributes(sx)$"scaled:center"
   }
+  
+  if(class(x)=="data.frame") {
+    sx = as.data.frame(sx,row.names=row.names(x))
+    names(sx) = names(x)
+  }
+  return(sx)
 }
 
 # #sf7  - calculate deviance from surface
